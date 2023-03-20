43a345e53e79f11f750d8bca62920f005286140c
Hammadulhaq123 <professorcoding123@gmail.com>
2023-03-20 21:59:07 +0500

    Opened the application and it was was throwing an error 'Cant find module prop-types' So I fixed the bug by just installing prop-type through npm other changes are complementary will review them later

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 
 export class Navbar extends Component {
 
-  
+
   render() {
     return (
 
@@ -14,7 +14,7 @@ export class Navbar extends Component {
             <img src="https://flowbite.com/docs/images/logo.svg" className="h-6 mr-3 sm:h-9" alt="" />
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">World News</span>
           </Link>
-          
+
           <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
             <ul className="flex flex-row justify-center align-middle p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li className='mt-2'>
@@ -23,13 +23,12 @@ export class Navbar extends Component {
               <li className='mt-2'>
                 <Link to="/about" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About Us</Link>
               </li>
-              <div className="relative hidden md:block">
-                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                  <svg className="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
-                  <span className="sr-only">Search icon</span>
-                </div>
-                <input type="text" id="search-navbar"  className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..."/>
-              </div>
+              <li>
+                <form>
+                  <input type="text" className='h-8 w-36 rounded pl-2' placeholder='Search News' />
+                  <button type='submit' className='bg-blue-700 text-white w-16 h-8 rounded ml-1'>Search</button>
+                </form>
+              </li>
 
             </ul>
           </div>
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 
 export class News extends Component {
@@ -23,22 +23,22 @@ export class News extends Component {
   constructor() {
     super();
     this.state = {
-      articles : [],
+      articles: [],
       loading: false,
-      page:1
+      page: 1
     }
   }
 
   async updateNews(pageNo) {
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=723fb939ad074eec8d4f5d7e46a66d3a&page=${this.state.page}&pagesize=${this.props.pageSize}`;
-    this.setState({loading : true});
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
     // console.log(parsedData)
-    this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults, loading: false});
+    this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults, loading: false });
   }
 
-  async componentDidMount(){
+  async componentDidMount() {
     // console.log("cdm");
     // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=723fb939ad074eec8d4f5d7e46a66d3a&page=1&pagesize=${this.props.pageSize}`;
     // this.setState({loading : true});
@@ -65,7 +65,7 @@ export class News extends Component {
     //     loading: false,
     //   })
     // }
-    this.setState({page: this.state.page + 1})
+    this.setState({ page: this.state.page + 1 })
     this.updateNews()
   }
   handlePrevPage = async () => {
@@ -84,7 +84,7 @@ export class News extends Component {
     //   loading: false,
     // })
 
-    this.setState({page: this.state.page - 1})
+    this.setState({ page: this.state.page - 1 })
     this.updateNews()
   }
   render() {
@@ -92,18 +92,18 @@ export class News extends Component {
       <div className='flex flex-col my-28 ml-60'>
         <h2 className='text-4xl text-center mb-8 text-white'>World News | {this.props.name}</h2>
         {this.state.loading && <Spinner />}
-        
+
         <div className="flex gap-3 mx-24 mt-5 flex-wrap">
-        
-          {!this.state.loading && this.state.articles.map((element)=>{
-            return(
-              <NewsItem key={element.url} author={element.author} date={element.publishedAt} title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url}/>
+
+          {!this.state.loading && this.state.articles.map((element) => {
+            return (
+              <NewsItem key={element.url} author={element.author} date={element.publishedAt} title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} />
             )
-          })} 
+          })}
         </div>
         <div className='flex justify-around align-middle my-20'>
-        <button disabled={this.state.page<=1} onClick={this.handlePrevPage} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Previous</button>
-        <button disabled={(this.state.page + 1 > Math.ceil(this.state.totalResults/this.state.pages))} onClick={this.handleNextPage} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Next</button>
+          <button disabled={this.state.page <= 1} onClick={this.handlePrevPage} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Previous</button>
+          <button disabled={(this.state.page + 1 > Math.ceil(this.state.totalResults / this.state.pages))} onClick={this.handleNextPage} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Next</button>
         </div>
       </div>
     )
